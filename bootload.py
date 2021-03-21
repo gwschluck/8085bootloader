fc7de6b4ac2cb496c5d4a021d66cb70b527fb125
Gary Schluckbier <gary@schluckbier.com>
2021-03-21 10:29:21 -0500

    Updated for 16bt operations and improved user feedback

--- bootload.py
+++ bootload.py
@@ -4,6 +4,16 @@ import serial
 import time
 import sys
 
+SerialPort = '/dev/tty.usbserial-1420'
+BaudRate = 1200
+InterByteDelay = 0.01
+# Output Full 16-bit address?
+FullAddress = True
+# Only Used if FullAddress = 0 -- when we send 8-bit address followed by 8-bit byte
+AddressMSB = 0x28
+Debug = False
+
+
 def read_bytes(filename):
     with open(filename, 'r') as fileobj:
         lines = fileobj.readlines()
@@ -15,16 +25,54 @@ def read_bytes(filename):
                 yield address+byte, int(line[9+byte*2: 11+byte*2], 16)
 
 
-with serial.Serial('/dev/tty.usbserial-1420', 1200, timeout=1) as ser:
-    
+with serial.Serial(SerialPort, BaudRate, timeout=1) as ser:
+
+    byte_seq = 0  # We went to print out 8 bytes at a time
+    prev_addr = 0
+
     for addr, byte in read_bytes(sys.argv[1]):
-        print ("Addr: {}, byte: {}".format(hex(addr), hex(byte)))
-        if (addr > 0x28FF) or (addr < 0x2800):
-            print ("Skip!")
+        
+        # Give user feedback
+        if (prev_addr + 1) != addr:
+            byte_seq = 0
+            print ("")
+        if byte_seq == 0:
+            print (" ADDR {:04X} :".format(addr), end = '')
+        if (byte_seq % 4) == 0:
+            print (" ", end = '')
+        print (" {:02X} ".format(byte), end = '')
+        byte_seq += 1;
+        if byte_seq == 16:
+            byte_seq = 0
+            print ("")
+        prev_addr = addr;
+
+        if Debug: 
+            print ("Addr: {:04X}, byte: {:02X}".format(addr, byte))
+        if not FullAddress and ((addr > ((AddressMSB<<8)|0xFF)) or (addr < (AddressMSB<<8))):
+            print (" Skipping Address: {:04X}!".format(addr))
+            byte_seq = 0;
         else:
-            print ("I would like to write {} {}".format(hex(addr&0xFF), hex(byte)))
-            a = ser.write(bytes([addr&0xff, byte]))
-            print ("Wrote {} Bytes".format(a))
+            if FullAddress:
+                if Debug:
+                    print ("I would like to write {:02X}".format((addr&0xFF00)>>8))
+                a = ser.write(bytes([((addr&0xff00)>>8)]))
+                time.sleep(InterByteDelay)
+            else:
+                a = 0
+
+            if Debug:
+                print ("I would like to write {:02X}".format(addr&0xFF))
+            a += ser.write(bytes([addr&0xff]))
+            time.sleep(InterByteDelay)
+            if Debug:
+                print ("I would like to write {:02X}".format(byte))
+            a += ser.write(bytes([byte]))
+            time.sleep(InterByteDelay)
+            if Debug:
+                print ("Wrote {} Bytes".format(a))
 
     ser.close()
+
+    print ("\n")
 
